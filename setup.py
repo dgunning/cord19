06ea494a2963c287e3549d7da12dfcf39eca1d57
Dwight Gunning <dgunning@gmail.com>
2020-03-19 10:23:57 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    REQUIRED = [l.strip() for l in f.readlines()]
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='cord19',
+    version='0.4.0',
+    description='COVID-19 Open Dataset Research Tool',
+    long_description=readme,
+    author='Dwight Gunning',
+    author_email='dgunning@gmail.com',
+    url='https://github.com/dgunning/cord19.git',
+    license=license,
+    packages=['cord'],
+    install_requires=REQUIRED
+)
