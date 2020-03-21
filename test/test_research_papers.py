2a4b6b9b9d6ef509b9d204ef1f39317a0d929794
Dwight Gunning <dgunning@gmail.com>
2020-03-21 17:37:07 -0400

    Add pytest

--- test/test_research_papers.py
+++ test/test_research_papers.py
@@ -1,3 +1,6 @@
+import pytest
+from cord.cord19 import ResearchPapers
 
-
-print('a')
+def test_load_research_papers():
+    print('Loading research papers')
+    reseearch_paper = ResearchPapers.from_data_dir()
