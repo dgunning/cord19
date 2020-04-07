d0f5984201e22a72ba540a54f597ba8134a5dbec
Dwight Gunning <dgunning@gmail.com>
2020-04-07 07:44:28 -0400

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-EXTRAS_REQUIRES = {'nlp': ['allennlp', 'torch===1.4.0', 'scispacy'],
+EXTRAS_REQUIRES = {'nlp': ['allennlp', 'torch==1.4.0', 'scispacy'],
                    'ui': ['streamlit']}
 EXTRAS_REQUIRES['all'] = EXTRAS_REQUIRES['nlp'] + EXTRAS_REQUIRES['ui']
 setup(
